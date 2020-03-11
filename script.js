ce40a515d0977f0f892babb57d202f6c6170b52a
Евгений Давидович <davidovichyauhen@gmail.com>
2020-03-11 17:51:46 +0300

    feat: complete toggle menu

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const Nav = document.getElementById("menu");
 Nav.addEventListener("click", (event) => {
-    Nav.querySelectorAll("li").forEach(el => el.classList.remove("current"));    
+    Nav.querySelectorAll("a").forEach(el => el.classList.remove("current"));    
     event.target.classList.add("current");
 });
