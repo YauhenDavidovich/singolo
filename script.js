29972264a16b067a853b8cb842d8598676a13049
Евгений Давидович <davidovichyauhen@gmail.com>
2020-03-11 23:31:52 +0300

    feat: add basic form message

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const Nav = document.getElementById("menu");
-Nav.addEventListener("click", (event) => {
-    Nav.querySelectorAll("a").forEach(el => el.classList.remove("current"));    
+const NAV = document.getElementById("menu");
+const BUTTON = document.getElementById("button");
+
+
+NAV.addEventListener("click", (event) => {
+    NAV.querySelectorAll("a").forEach(el => el.classList.remove("current"));    
     event.target.classList.add("current");
+});
+
+BUTTON.addEventListener("click", () => {
+    const subject = document.getElementById("subject").value.toString();
+    document.getElementById("result").innerText = subject;
+    document.getElementById("message-block").classList.remove("hidden"); 
+
 });
